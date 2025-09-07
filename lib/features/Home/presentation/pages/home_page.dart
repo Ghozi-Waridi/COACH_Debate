ab935a52e0ca738366f2ae67e4fe2d2d40e43970
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-09-07 22:00:50 +0700

    :star: ADD: Feature Home

---
+++ lib/features/Home/presentation/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Selamat Datang Ghozi'),
+            Text('Mari Kita Mulai Debat'),
+            InkWell(
+              child: CircleAvatar(child: Container(color: Colors.blue)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
